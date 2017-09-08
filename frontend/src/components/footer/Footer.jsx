8ba7a5fd4257bfa0de738a44f128bdf9f2bd830c
kim borgen <kimatborgen@gmail.com>
2017-09-08 22:37:48 +0200

    add footer with links, copyrigth & styling, add font-awesome for icons

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -5,9 +5,16 @@ export default class Footer extends Component {
   render() {
     return (
         <footer className="footer">
-			<p>
-				a
-			</p>
+            <div className="footer__links">
+                <a href="https://github.com/kimborgen" className="fa fa-github"></a>
+                <a href="https://www.facebook.com/kimatborgen" className="fa fa-facebook"></a>
+                <a href="https://www.linkedin.com/in/kim-borgen-76b493146/" className="fa fa-linkedin"></a>
+                <a href="mailto:ceo@kimborgen.no" className="fa fa-envelope"></a>
+                <a href="https://www.snapchat.com/add/kimborgen" className="fa fa-snapchat-ghost"></a>
+                <a href="https://www.youtube.com/user/hohohehe321" className="fa fa-youtube"></a>
+            </div>
+
+			<p className="footer__copyrigth">&copy; Kim Borgen 2017 <i className="fa fa-long-arrow-right fa-lg" aria-hidden="true"></i> <div className="footer__copyrigth__infinte">&infin;</div></p>
 		</footer>
     );
   }
