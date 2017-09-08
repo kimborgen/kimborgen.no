1008b17d7ba0722bb7b5bbe8879ec424455a22b3
kim borgen <kimatborgen@gmail.com>
2017-09-09 00:45:07 +0200

    add temp header and change site layout from grid to flex

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -6,15 +6,20 @@ import Footer from '../footer/Footer';
 export default class Home extends Component {
   render() {
 	  var hello = []
-	  hello.push("hei");
+      /*
+      for (let i = 0; i < 100; i++) {
+          hello.push(<div style={{color:"red"}}>hei</div>)
+      }
+      */
+
     return (
         <div className="home">
-			<Nav />
-			<p>
-			{hello.map((h) => {
-				return h
-			})}
-			</p>
+            <header className="home__header">
+                <Nav />
+            </header>
+            <div className="content">
+                
+            </div>
 			<Footer />
 		</div>
     );
