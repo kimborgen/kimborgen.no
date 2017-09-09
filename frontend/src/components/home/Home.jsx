3c5134e3df7f532ff495a40083c56729c465acdd
kim borgen <kimatborgen@gmail.com>
2017-09-09 14:07:00 +0200

    fix header

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -17,6 +17,7 @@ export default class Home extends Component {
         <div className="home">
             <header className="home__header">
                 <Nav />
+                <img src="tmp_header.png"></img>
             </header>
             <div className="content">
                 <About />
