360b0c230c6a771b2e4811380ef4e9aadfc60c55
kim borgen <kimatborgen@gmail.com>
2017-09-11 03:51:45 +0200

    add top of CV: Personal Statement, Education & fancy skills section

--- frontend/src/components/cv/CV.jsx
+++ frontend/src/components/cv/CV.jsx
@@ -11,11 +11,40 @@ export default class CV extends Component {
 	  */
 
     return (
-		<div className="cv__wrapper">
+		<div className="wrapper__cv">
 	        <section className="cv">
-	            <h1>Curriculum Vitae</h1>
+	            <h1>Kim Borgen</h1>
+				<p className="header__text">Visit <a href="kimborgen.no">kimborgen.no</a> for more information</p>
+				<h2 className="personalStatement">Personal Statement</h2>
+				<p className="personalStatement__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur bsDuis aute irure</p>
+				<div className="divider__top"></div>
+				<h2 className="education">Education</h2>
+				<div className="education__wrapper">
+					<div className="education__degree"><b>M.S in Computer Science & Engineering, 2015-2020, at </b></div>
+					<a href="www.ntnu.edu">The Norwegian University of Science and Technology [NTNU]</a>
+				</div>
+				<h2 className="skills">Skills</h2>
+				<div className="skills__wrapper">
+					<img src="skills_circle.svg"></img>
+					<img src="skills_circle.svg"></img>
+					<img src="skills_circle.svg"></img>
+					<img src="skills_circle.svg"></img>
+					<img src="skills_circle.svg"></img>
+					<img src="skills_circle.svg"></img>
+					<div></div><div></div>
+					<p>Html, css & js</p>
+					<p>Django & rails</p>
+					<p>Python, go & java</p>
+					<p>Servers & sysadmin</p>
+					<p>Graphical Design</p>
+					<p>Film, editing & 3D</p>
+
+
+
+				</div>
 			</section>
 		</div>
     );
   }
+  //jeg har en fin penis
 }
