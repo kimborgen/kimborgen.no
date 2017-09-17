5ecd19b24f12ec1c55c6106149c8fc553442e556
kim borgen <kimatborgen@gmail.com>
2017-09-17 22:59:54 +0200

    add basic graphql (#rip REST) setup

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 'use strict';
 
 const express = require('express');
-const app = express();
+var app = express();
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
 
 // Use the built-in express middleware for serving static files from './public'
 app.use(express.static('build'));
 app.use(express.static('build/static'));
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
 
 const server = app.listen(8080, () => {
   const host = server.address().address;
