cdd9a2d799b6a934f707035fa19580da3b23663a
kim borgen <kimatborgen@gmail.com>
2017-09-17 16:16:50 +0200

    fix cv

--- src/components/cv/CV.jsx
+++ src/components/cv/CV.jsx
@@ -19,13 +19,13 @@ export default class CV extends Component {
 
 				<div className="cvPicture"></div>
 
-                <h2 className="intrests">Main areas of intrests</h2>
+                <h2 className="intrests">Main areas of interest</h2>
                 <ul className="intrests__bullet1">
                     <li>Blockchain & Ethereum/Solidity</li>
                     <li>Artificial Intelligence</li>
                 </ul>
                 <ul className="intrests__bullet2">
-                    <li>Full-stack web-development</li>
+                    <li>Full Sstack Web Development</li>
                     <li>Marketing & Design</li>
                 </ul>
 
@@ -60,15 +60,15 @@ export default class CV extends Component {
 
                 <p className="experiences__ascend__date">July 2017 - Present</p>
                 <h3 className="experiences__ascend__header">Communication Leader at <a href="http://ascendntnu.no" target="newtab">Ascend NTNU</a></h3>
-                <p className="experiences__ascend__description">In charge of communcation & marketing for the voluntary based student organization <a href="http://ascendntnu.no">Ascend NTNU</a>. Head of the communcation group with 4 people. I am also a board member. It is my task to bring Ascend NTNU's image to new heights. Not only for the students at NTNU but also for businesses and press nationwide. To do that I dvelve into the areas of social media marketing, sponsorships, events, graphical design, web development & design and brand building.</p>
+                <p className="experiences__ascend__description">In charge of communcation & marketing for the voluntee based student organization <a href="http://ascendntnu.no">Ascend NTNU</a>. We compete in the most prestigious international competition for university students in aerial robotics. I am in charge of the communcation group of 4 people and I am also a board member. It is my task to bring Ascend NTNU's image to new heights, not only for the students at NTNU but also for businesses and the media. This involves dvelving into the areas of social-media marketing, sponsorships & events, graphic design, film & photo, web-design and brand building.</p>
 
                 <p className="experiences__xcom__date">March 2017 - Present</p>
-                <h3 className="experiences__xcom__header">Full-stack web-developer & sysadmin at <a href="https://xcom18.abakus.no" target="newtab">XCOM 2018</a></h3>
-                <p className="experiences__xcom__description">Sole developer and sysadmin for the exursion comitee at Abakus, a student organization for MSc's in CS & CommTech. I refreshed the design on our website, and I am currrently responsible for further web-development, data managment, and server setup & maintenance. The website can be visited at <a href="https://xcom18.abakus.no" target="newtab">xcom18.abakus.no</a></p>
+                <h3 className="experiences__xcom__header">Full Stack Web Developer & Sysadmin at <a href="https://xcom18.abakus.no" target="newtab">XCOM 2018</a></h3>
+                <p className="experiences__xcom__description">Developer and sysadmin for the excursion commitee at Abakus, a student organization for MScs in CS & CommTech. I refreshed the design on our website, and I am currrently responsible for further web-development, data managment, and server setup & maintenance. The website can be visited at <a href="https://xcom18.abakus.no" target="newtab">xcom18.abakus.no</a>. <i>Keywords: CSS, Django & Server</i></p>
 
                 <p className="experiences__isfit__date">April 2016 - March 2017</p>
-                <h3 className="experiences__isfit__header">Full-stack web-developer at <a href="https://isfit.org" target="newtab">ISfIT 2017</a></h3>
-                <p className="experiences__isfit__description">A member of the IT group at the <a href="https://isfit.org" target="newtab">ISfIT festival in 2017</a>. Worked with a team of 10 people. My main area of responsibility was developing our internal site with another developer, this internal site served close to 500 people.</p>
+                <h3 className="experiences__isfit__header">Full Stack Web Developer at <a href="https://isfit.org" target="newtab">ISfIT 2017</a></h3>
+                <p className="experiences__isfit__description">I was a member of the IT group at the <a href="https://isfit.org" target="newtab">ISfIT festival in 2017</a>, working with a team of 10 people. My main area of responsibility was developing our internal site with another developer. This internal site served close to 500 people. <i>Keywords: Rails, JS, CSS</i></p>
 
                 <div className="divider__experiences__bottom"><div></div></div>
 
@@ -77,17 +77,17 @@ export default class CV extends Component {
 
                 <h2 className="projects">Projects</h2>
                 <ul className="projects__list">
-                    <li><b><a href="https://stelios.no" target="newtab">Stelios.no - </a></b> Senior developer in a team of 4. Developing an idea from scratch to a finnished product in the course <a href="http://www.ntnu.edu/studies/courses/TDT4140#tab=omEmnet" target="newtab">Software Engineering</a>. Website made with react and django rest api</li>
-                    <li><b><a href="http://kimborgen.no" target="newtab">kimborgen.no - </a></b> My personal webpage. Frontend is made with react and the design is purely made in css. I also made this cv as part of the website. Backend is a nodejs server running on google cloud</li>
-                    <li><b>Decentralized exchange built on ethereum - </b> The exchange is built upon smart contract code in ethereum's language solidity. Details still secret, stay tuned!</li>
+                    <li><b><a href="https://stelios.no" target="newtab">Stelios.no - </a></b> Senior developer in a team of 4. Developing an idea from scratch to a finished product on the course <a href="http://www.ntnu.edu/studies/courses/TDT4140#tab=omEmnet" target="newtab">"Software Engineering"</a>. <i>Keywords: React, Django REST api, CSS, Continuous Integration, Servers, Testing, LEAN, Travis</i></li>
+                    <li><b><a href="http://kimborgen.no" target="newtab">kimborgen.no - </a></b> My personal webpage. The frontend is made with React and the design is entirely made in CSS. I also made this CV as part of the website. The backend is a NodeJS server running on Google Cloud</li>
+                    <li><b>Decentralized exchange built on Ethereum (in progress) - </b> The exchange is built upon smart contract code in Ethereum's programming language Solidity. </li>
                 </ul>
 
                 <h2 className="contact">Contact Information</h2>
                 <ul className="contact__list">
-                    <li>Trondheim & Oslo (Norway)</li>
                     <li><a href="https://github.com/kimborgen" target="newtab">github.com/kimborgen</a></li>
                     <li><a href="mailto:ceo@kimborgen.no">ceo@kimborgen.no</a></li>
                     <li><a href="http://kimborgen.no" target="newtab">kimborgen.no</a></li>
+                    <li>24-05-1996</li>
                     <li>48357052</li>
                 </ul>
 
