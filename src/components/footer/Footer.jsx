6e379dc0c17df0033f61b24e47ee4bab1c030374
Kim Borgen <kimatborgen@gmail.com>
2017-12-12 03:23:47 +0100

    update linkdin link

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,7 @@ export default class Footer extends Component {
             <div className="footer__links">
                 <a href="https://github.com/kimborgen" className="fa fa-github" target="newtab"></a>
                 <a href="https://www.facebook.com/kimatborgen" className="fa fa-facebook" target="newtab"></a>
-                <a href="https://www.linkedin.com/in/kim-borgen-76b493146/" className="fa fa-linkedin" target="newtab"></a>
+                <a href="https://www.linkedin.com/in/kimborgen" className="fa fa-linkedin" target="newtab"></a>
                 <a href="mailto:ceo@kimborgen.no" className="fa fa-envelope" target="newtab"></a>
                 <a href="https://www.snapchat.com/add/kimborgen" className="fa fa-snapchat-ghost" target="newtab"></a>
                 <a href="https://www.youtube.com/user/hohohehe321" className="fa fa-youtube" target="newtab"></a>
