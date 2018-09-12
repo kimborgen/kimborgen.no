ee8265dc5f0531112dd44612cda3335760567888
kim borgen <kim.borgen@dexternal.com>
2018-09-12 16:26:13 +0200

    Center stuff, fix spacing and clean up

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,7 +9,6 @@ export default class About extends Component {
     return (
       <section className='about'>
         <Element className='element' name='scrollToAbout' />
-        <h1 />
         <img src='blockchain-ai.png' />
 
         <article>
@@ -17,8 +16,6 @@ export default class About extends Component {
           <p>I believe emerging technologies such as <b>blockchain</b> and <b>artificial intelligence</b> has the ability to revolutionize and <b>disrupt</b> the status quo. These technologies allows us not only to rebuild old and uneffective systems, but to create new, <b>innovative</b> and <b>creative solutions</b> that will usher in a brand new globalized, automized and <b>decentralized</b> age.
           </p>
         </article>
-
-        <div className='about__divider' />
       </section>
     )
   }
