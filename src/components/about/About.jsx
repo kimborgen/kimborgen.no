933de0dd82a59787c658b9206fc538e7941ef220
kim borgen <kim.borgen@dexternal.com>
2018-09-12 16:02:18 +0200

    Update passion desc

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,7 +14,8 @@ export default class About extends Component {
 
         <article>
           <h2>Passion</h2>
-          <p>I believe emerging technologies such as <b>blockchain</b> and <b>artificial intelligence</b> has the ability to revolutionize and <b>disrupt</b> the current way of life. These technologies allows us not only to rebuild old and uneffective systems, but to create new, <b>innovative</b> and <b>creative</b> solutions that benefit <b>everyone</b> and will usher in a brand new globalized, automized and <b>decentralized</b> age.</p>
+          <p>I believe emerging technologies such as <b>blockchain</b> and <b>artificial intelligence</b> has the ability to revolutionize and <b>disrupt</b> the status quo. These technologies allows us not only to rebuild old and uneffective systems, but to create new, <b>innovative</b> and <b>creative solutions</b> that will usher in a brand new globalized, automized and <b>decentralized</b> age.
+          </p>
         </article>
 
         <div className='about__divider' />
