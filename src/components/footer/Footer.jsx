78664531012d99b0ee496cc56481fa114feb306e
Kim Borgen <me@kimborgen.com>
2018-09-27 21:43:28 +0200

    Update email and year

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,12 +9,12 @@ export default class Footer extends Component {
             <a href='https://github.com/kimborgen' className='fa fa-github' target='newtab' />
             <a href='https://www.facebook.com/kimatborgen' className='fa fa-facebook' target='newtab' />
             <a href='https://www.linkedin.com/in/kimborgen' className='fa fa-linkedin' target='newtab' />
-            <a href='mailto:hi@kimborgen.com' className='fa fa-envelope' target='newtab' />
+            <a href='mailto:me@kimborgen.com' className='fa fa-envelope' target='newtab' />
             <a href='https://www.snapchat.com/add/kimborgen' className='fa fa-snapchat-ghost' target='newtab' />
             <a href='https://www.youtube.com/user/hohohehe321' className='fa fa-youtube' target='newtab' />
           </div>
 
-        <p className='footer__copyrigth'>&copy; Kim Borgen 2017 <i className='fa fa-long-arrow-right fa-lg infarrow' aria-hidden='true' /> <div className='footer__copyrigth__infinte'>&infin;</div></p>
+        <p className='footer__copyrigth'>&copy; Kim Borgen 2018 <i className='fa fa-long-arrow-right fa-lg infarrow' aria-hidden='true' /> <div className='footer__copyrigth__infinte'>&infin;</div></p>
       </footer>
     )
   }
