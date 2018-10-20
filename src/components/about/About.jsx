143d56550c345cb0440489b1db9dd7fb00476053
Kim Borgen <me@kimborgen.com>
2018-10-20 02:03:46 +0200

    fix spelling and css

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -13,7 +13,7 @@ export default class About extends Component {
 
         <article>
           <h2>Passion</h2>
-          <p>I believe emerging technologies such as <b>blockchain</b> and <b>artificial intelligence</b> has the ability to revolutionize and <b>disrupt</b> the status quo. These technologies allows us not only to rebuild old and uneffective systems, but to create new, <b>innovative</b> and <b>creative solutions</b> that will usher in a brand new globalized, automized and <b>decentralized</b> age.
+          <p>I believe emerging technologies such as <b>blockchain</b> and <b>artificial intelligence</b> has the ability to revolutionize and <b>disrupt</b> the status quo. These technologies allows us to create new, <b>innovative</b> and <b>creative solutions</b> that will usher in a brand new globalized, automized and <b>decentralized</b> age.
           </p>
         </article>
       </section>
