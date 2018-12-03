39f3723c8fd14b5c34f79618e4a7388b5dfc6c4a
Kim Borgen <me@kimborgen.com>
2018-12-03 01:33:36 +0100

    Replace embedded instagram with selfmade

--- src/components/instafeed/SingleInstaPost.jsx
+++ src/components/instafeed/SingleInstaPost.jsx
@@ -2,52 +2,67 @@ import React, { Component } from 'react';
 import './InstaFeed.css';
 
 export default class SingleInstaPost extends Component {
-    constructor() {
-        super()
-        this.state = {
-            embedded: undefined
-        }
+  constructor() {
+      super()
+      this.state = {
+      }
+  }
+
+  /* Here i used to use the embedded instagram html gotten from insta's api
+   * Since these components is rendered in an iframe, it was imposible to manipulate
+   * those objects. Meaning that for example scrolling did not work. 
+  */ 
+
+  componentDidMount() {
+    console.log(this.props.data)
+  }
+
+  imageOrVideo = () => {
+    if (this.props.data.type === "video") {
+      return <video 
+        className="singleinstapost__video"
+        controls 
+        playsInLine 
+        autoPlay 
+        muted 
+        loop 
+        src={this.props.data.videos.standard_resolution.url}
+        type="video/mp4"
+      />
+    } else {
+      return <img
+        className="singleinstapost__image"
+        src={this.props.data.images.standard_resolution.url}
+      />
     }
-
-    componentDidMount() {
-        fetch("https://api.instagram.com/oembed/?url=" + this.props.data.link)
-        .then(response => {
-            return response.json()
-        })
-        .then(result => {
-            console.log(result)
-            this.setState({
-                embedded: result.html
-            })
-        })
-
-    }
-
-    componentDidUpdate() {
-        if (this.state.embedded === undefined) {
-            console.log("embedded state was undefined")
-        } else {
-            window.instgrm.Embeds.process();
-            console.log("Embedded.process was called")
-        }
-    }
-
-
-    
-
-    createHtml = () => {
-        return {
-            __html: this.state.embedded
-        };
-    }
+  }
 
   render() {
-      if (this.state.embedded === undefined) {
-          return (<p>loading</p>)
-      } else {
-          return (
-            <div className="singleinstapost" dangerouslySetInnerHTML={this.createHtml()} />
-          );
-      }
+    return (
+      <a href="{this.props.data.link}" className="singleinstapost">
+        {this.imageOrVideo()}
+        <div className="singleinstapost__more">
+          <p className="singleinstapost__location">
+            { 
+            
+              this.props.data.location === null ?
+
+                ""
+
+                :
+
+                this.props.data.location.name
+
+            }
+          </p>
+          <p className="singleinstapost__likes">
+            {this.props.data.likes.count}
+          </p>
+          <p className="singleinstapost__caption">
+            {this.props.data.caption.text}
+          </p>
+        </div>  
+      </a>
+    );
   }
 }
