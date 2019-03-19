ed2ec38cf4189f255271732bb8a440f0dddbe82d
Kim Borgen <me@kimborgen.com>
2019-03-19 17:09:06 +0100

    Add business cv

--- src/components/businessCV/CV.jsx
+++ src/components/businessCV/CV.jsx
@@ -6,14 +6,12 @@ import { withRouter } from 'react-router'
 var Scroll = require('react-scroll')
 var Element = Scroll.Element
 
-class CV extends Component {
+class BusinessCV extends Component {
   render () {
-    console.log(this.props.location.pathname)
-    console.log(this.props.location.pathname === '/CV')
     return (
       <div className='wrapper__cv'>
         <Element className='element' name='scrollToCV' />
-        { this.props.location.pathname.toLowerCase() === '/cv' || this.props.location.pathname.toLowerCase() === '/businesscv' ? undefined : <div className="cvlinks">Click for printable CV's: <Link to='/cv'>Standard Tech CV</Link><Link to='/businesscv'>Business CV</Link></div> }
+        { this.props.location.pathname.toLowerCase() === '/cv' || this.props.location.pathname.toLowerCase() === '/businesscv' ? undefined : <div className="cvlinks">Click for a printable CV page<Link to='/cv' className='cvlink'>Standard tech CV</Link><Link to='/businesscv'>Business CV</Link></div> }
         <section className='cv'>
           <h1>Kim Aksel Tahuil Borgen</h1>
 
@@ -46,12 +44,12 @@ class CV extends Component {
             <img src='skills_circle.svg' />
             <img src='skills_circle.svg' />
             <div /><div />
-            <p>Frontend & graphical enviroments</p>
-            <p>Backend, Web Server & Software Architecture</p>
-            <p>Python, JavaScript, Go, C++ & Java</p>
-            <p>Project & Team Managment</p>
-            <p>Video production & graphical design</p>
-            <p>Smart Contracts and Decentralized Architecture</p>
+            <p>Business development and leadership</p>
+            <p>Analytical reasoning and research </p>
+            <p>Project planning, scrum and team managment</p>
+            <p>Video production, photography and graphical design</p>
+            <p>Automation, software architecture and development</p>
+            <p>Blockchain applications and international financial law</p>
           </div>
 
           <h2 className='experiences'>Experiences</h2>
@@ -60,7 +58,6 @@ class CV extends Component {
 
           <p className='experiences__ascend__date'>July 2017 - August 2018</p>
 
-          <p className='experiences__isfit__date'>April 2016 - March 2017</p>
 
           <div className='experiences__text'>
 
@@ -70,8 +67,6 @@ class CV extends Component {
             <h3 className='experiences__ascend__header'>Head of Marketing & Communication at <a href='https://ascendntnu.no' target='newtab'>Ascend NTNU</a></h3>
             <p className='experiences__ascend__description'>In charge of marketing and communication at Ascend NTNU, a student organization that compete internationally with autonomous aerial robotics. I was in charge of the marketing group of 4 people and I was also a board member. Our work yielded up to 120% more applicants in our different recruiting phases, compared to the year before.</p>
 
-            <h3 className='experiences__isfit__header'>Full Stack Web Developer at <a href='https://isfit.org' target='newtab'>ISFiT 2017</a></h3>
-            <p className='experiences__isfit__description'>I was a member of the IT group at the <a href='https://isfit.org' target='newtab'>ISFiT festival in 2017</a>, working with a team of 10 people. My main area of responsibility was developing our internal site with another developer. This internal site served close to 500 people. </p>
           </div>
 
           <div className='divider__experiences__bottom'><div /></div>
@@ -83,12 +78,13 @@ class CV extends Component {
             <li>Technical group member - Film & Editing - Abakus Revue 2019</li>
             <li>Scene group member - Abakus Revue 2018</li>
             <li>Full Stack Web Developer & Sysadmin <br/>&emsp; at <a href='https://xcom19.abakus.no' target='newtab'>XCOM 2018</a>  (March 2017 - May 2018)</li>
-            <li>Summer assistant at the produce section <br/> &emsp; at Coop Obs hypermarked Elverum (Summer 2013)</li>
+            <li>Full Stack Web Developer <br />&emsp; at <a href='https://isfit.org' target='newtab'>ISFiT 2017</a> (April 2016 - March 2017)</li>
+            <li>Summer assistant at the produce section <br/> &emsp; at Coop Obs Hypermarked Elverum (Summer 2013)</li>
           </ul>
 
           <h2 className='contact'>Contact Information</h2>
           <ul className='contact__list'>
-            <li><a href='https://github.com/kimborgen' target='newtab'>github.com/kimborgen</a><img src="https://image.flaticon.com/icons/svg/25/25231.svg" /></li>
+            <li><a href='https://www.linkedin.com/in/kimborgen/' target='newtab'>linkedin.com/in/kimborgen</a><img src="https://www.crevita.no/wp-content/uploads/2016/01/LinkedIn-logo-300x300.png" /></li>
             <li><a href='mailto:me@kimborgen.com'>me@kimborgen.com</a> <i className="material-icons">email</i></li>
             <li><a href='http://kimborgen.com' target='newtab'>kimborgen.com</a> <i className="material-icons">public</i></li>
             <li>24-05-1996 <i className="material-icons">cake</i></li>
@@ -101,4 +97,4 @@ class CV extends Component {
   }
 }
 
-export default withRouter(CV)
+export default withRouter(BusinessCV)
