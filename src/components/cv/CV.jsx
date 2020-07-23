7c4688cb32906ec417298f4635456ee761d7363c
Kim Borgen <me@kimborgen.com>
2020-07-23 23:05:34 +0200

    (old) Update cv to current

--- src/components/cv/CV.jsx
+++ src/components/cv/CV.jsx
@@ -40,24 +40,24 @@ class CV extends Component {
           <h2 className='skills'>.</h2>
           <h2 className='skills_real'>Skills</h2>
           <div className='skills__wrapper'>
+            <p>Blockchain app development</p>
+            <p>Blockchain scalability</p>
             <p>AI and ML development</p>
             <p>Big data and data processing</p>
-            <p>Blockchain and applications</p>
             <p>Software architechure</p>
             <p>Full stack web development</p>
-            <p>European financial law</p>
             <div /><div />
             <p>Leadership and managment</p>
             <p>Scientific research and writing</p>
             <p>Project planning and scrum</p>
-            <p>Python, Go, JS, C++, Java ...</p>
+            <p>Python, Go, JS, CSS, C++, Java ...</p>
             <p>Numpy, Pandas, Pytorch, Unix ...</p> 
             <p>Solidity, React, QT, and Django</p>
           </div>
 
           <h2 className='experiences'>Experiences</h2>
 
-          <p className='experiences__dexternal__date'>June 2017 - Present</p>
+          <p className='experiences__dexternal__date'>June 2017 - February 2020</p>
 
           <p className='experiences__ascend__date'>June 2019 - August 2019</p>
 
@@ -66,12 +66,12 @@ class CV extends Component {
           <div className='experiences__text'>
 
             <h3 className='experiences__dexternal__header'>Founder and CEO at <a href='https://dexternal.com' target='newtab'>Dexternal</a></h3>
-            <p className='experiences__dexternal__description'>Dexternal is a blockchain startup dedicated to connect blockchain assets to the regulated environment of the real world through a fully decentralized crypto asset exchange compliant with Norwegian and international law. As a CEO I work with business development, strategy, law, funding, project planning and management of our development team. I am also researching theoretical solutions, designing decentralized architectures and implementing them as smart contracts on the Ethereum blockchain in Solidity.</p>
+            <p className='experiences__dexternal__description'>Dexternal was a blockchain startup dedicated to connect blockchain assets to the regulated environment of the real world through a fully decentralized crypto asset exchange compliant with Norwegian and international law. As a CEO I worked with business development, strategy, law, funding, project planning and management of our development team. I was also researching theoretical solutions, designing decentralized architectures and implementing them as smart contracts on the Ethereum blockchain in Solidity.</p>
 
             <h3 className='experiences__ascend__header'>Machine Learning Developer (Summer Intern) at <a href="https://infrontfinance.com">Infront</a></h3>
             <p className='experiences__ascend__description'>Developed on a machine learning pipeline to automate tedious human tasks using the deep learning framework PyTorch, and various other data science libraries. Also developed web tools for data analysis and labeling.</p>
 
-            <h3 className='experiences__isfit__header'>Head of Marketing & Communication at <a href='https://ascendntnu.no' target='newtab'>Ascend NTNU</a></h3>
+            <h3 className='experiences__isfit__header'>Head of Marketing & Communication (Volunteer) at <a href='https://ascendntnu.no' target='newtab'>Ascend NTNU</a></h3>
             <p className='experiences__isfit__description'>In charge of marketing and communication at Ascend NTNU, a student organization that compete internationally with autonomous aerial robotics. I was in charge of the marketing group of 4 people and I was also a board member. Our work yielded up to 120% more applicants in our different recruiting phases, compared to the year before.</p>
           </div>
 
@@ -81,10 +81,9 @@ class CV extends Component {
 
           <h2 className='projects'>Other</h2>
           <ul className='projects__list'>
-            <li>Scene builder and technical worker - Film & Editing - <br />&emsp; at the Abakus Revue 2018, 2019 and 2020</li>
-            <li>Full Stack Web Developer & Sysadmin <br/>&emsp; at <a href='https://xcom19.abakus.no' target='newtab'>XCOM 2018</a>  (March 2017 - May 2018)</li>
-            <li>Full Stack Web Developer <br/>&emsp; at <a href='https://isfit.org' target='newtab'>ISFiT 2017</a> (April 2016 - March 2017)</li>
-
+            <li>Scene builder and technical worker with Film & Editing (Volunteer) <br />&emsp; at the Abakus Revue 2018, 2019 and 2020</li>
+            <li>Full Stack Web Developer & Sysadmin (Volunteer) <br/>&emsp; at <a href='https://xcom19.abakus.no' target='newtab'>XCOM 2018</a>  (March 2017 - May 2018)</li>
+            <li>Full Stack Web Developer (Volunteer) <br/>&emsp; at <a href='https://isfit.org' target='newtab'>ISFiT 2017</a> (April 2016 - March 2017)</li>
             <li>Summer assistant at the produce section <br/> &emsp; at Coop Obs hypermarked Elverum (Summer 2013)</li>
           </ul>
 
@@ -93,8 +92,8 @@ class CV extends Component {
             <li><a href='https://github.com/kimborgen' target='newtab'>github.com/kimborgen</a><img src="https://image.flaticon.com/icons/svg/25/25231.svg" /></li>
             <li><a href='mailto:me@kimborgen.com'>me@kimborgen.com</a> <i className="material-icons">email</i></li>
             <li><a href='http://kimborgen.com' target='newtab'>kimborgen.com</a> <i className="material-icons">public</i></li>
+            <li>+47 48357052 <i className="material-icons">phone</i></li>
             <li>24-05-1996 <i className="material-icons">cake</i></li>
-            <li>48357052 <i className="material-icons">phone</i></li>
           </ul>
 
         </section>
